e619d86b713a61b1965de5c9ed6db3fd28922b4c
x214227202 <x214227202@users.noreply.github.com>
2026-04-09 10:31:45 -0700

    Initial Commit

---
+++ maccode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  maccode
+//
+//  Created by zwl on 2026/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ maccode/maccodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  maccodeApp.swift
+//  maccode
+//
+//  Created by zwl on 2026/4/9.
+//
+
+import SwiftUI
+
+@main
+struct maccodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
